61288fcd6cdabdd5d4cbd8a95932b5099b2908fb
Vishal Tiwari <vishaltiwarid54@gmail.com>
2017-11-09 15:10:00 +0530

    Implementing Collections

--- src/wordcount/Count.java
+++ src/wordcount/Count.java
@@ -1,5 +1,7 @@
 package wordcount;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Count {
@@ -11,27 +13,28 @@ public class Count {
 	}
 
 	public String[] extract(String str) {
-		String[] result = new String[10];
+		ArrayList<String> result = new ArrayList<>();
+		//String[] result = new String[10];
 		StringBuilder sb = new StringBuilder();
-		int j = 0;
 		char[] c = str.toCharArray();
 		for (int i = 0; i < c.length; i++) {
 			if (c[i] == ' ') {
-				result[j] = sb.toString();
+				result.add(sb.toString());
 				sb = new StringBuilder();
-				j++;
 			} else {
 				sb.append(c[i]);
 			}
-			result[j] = sb.toString();
+			
 		}
-		return result;
+		result.add(sb.toString());
+		return result.toArray(new String[result.size()]);
 	}
 
 	public WordCount[] count(String[] result) {
 		int count = 0;
+		String[] str = {"C","Java","rain","count"};
 		String word = "";
-		WordCount[] arr = new WordCount[10];
+		ArrayList<WordCount> arr = new ArrayList<>();
 		int pos = 0;
 		for (int i = 0; i < result.length; i++) {
 			word = result[i];
@@ -44,12 +47,14 @@ public class Count {
 					result[j] = null;
 				}
 			}
-			arr[pos] = new WordCount(word, count);
+			
+			if(count>0)
+				arr.add(new WordCount(word, count));
 			pos++;
 			word = "";
 			count = 0;
 		}
-		return arr;
+		return arr.toArray(new WordCount[arr.size()]);
 	}
 
 	public WordCount[] sort(WordCount[] arr) {
@@ -57,9 +62,9 @@ public class Count {
 			for (int j = i; j < arr.length; j++) {
 				WordCount first = arr[i];
 				WordCount second = arr[j];
-				if (arr[i] == null || arr[j] == null) {
-					continue;
-				}
+//				if (arr[i] == null || arr[j] == null) {
+//					continue;
+//				}
 				int comp = first.word.compareToIgnoreCase(second.word);
 				if (comp > 0) {
 					arr[i] = second;
--- src/wordcount/Word.java
+++ src/wordcount/Word.java
@@ -1,17 +1,16 @@
 package wordcount;
+
 import java.util.*;
 
 public class Word {
-	public static void main(String args[]){
+	public static void main(String args[]) {
 		Count c = new Count();
-		String[] result = c.extract("This is a count is word a program word");
+		String[] result = c.extract("This is a count is teach C c and Java but rain so no C java");
 		WordCount[] count = c.count(result);
 		WordCount[] sort = c.sort(count);
 		for (int i = 0; i < sort.length; i++) {
 			System.out.println(sort[i]);
 		}
-		
-		
-	
+
 	}
 }
